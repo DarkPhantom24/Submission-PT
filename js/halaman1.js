7eec3a425150a847388bfa7806bc148fb55944e6
aqilfathurrahman <aqiel011@gmail.com>
2025-06-13 10:59:57 +0800

    ini lagi script js untuk halaman 1 nya atau index itu

---
+++ js/halaman1.js
@@ -0,0 +1,9 @@
+var masuk = document.querySelector("button");
+masuk.addEventListener("click", function () {
+  alert("mohon maaf fitur ini akan tersedia secepatnya");
+});
+
+var daftar = document.querySelector(".btn");
+daftar.addEventListener("click", function () {
+  alert("untuk saat ini pendaftaran masih ditutup");
+});
